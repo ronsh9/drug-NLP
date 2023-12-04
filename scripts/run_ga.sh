83005eef51c3da4d955808fe4f49d671fe189927
thatprogrammer1 <thatprogrammer1@gmail.com>
2023-12-04 17:49:45 -0500

    log output to a results file

--- scripts/run_ga.sh
+++ scripts/run_ga.sh
@@ -1,5 +1,5 @@
 # run on our own database
-python mol_opt/run.py graph_ga --smi_file eval/data/example.tab --output_dir eval/results --task production --n_runs 1 --oracles qed
+python mol_opt/run.py graph_ga --smi_file eval/data/example.tab --output_dir eval/results --task production --n_runs 1 --oracles qed > eval/results/out.txt
 
 # run on the ZINC database
 # python mol_opt/run.py graph_ga --output_dir eval/results --task production --n_runs 1 --oracles qed
