c6bb7308f869d9dcd99d8bbdfcd5236578737270
thatprogrammer1 <thatprogrammer1@gmail.com>
2023-12-05 02:26:14 -0500

    add engaging script

--- scripts/run_ga.sh
+++ scripts/run_ga.sh
@@ -1,3 +1,4 @@
 # run
-python mol_opt/run_drug_nlp.py final_finetuning/results/10_production_experiment.csv  graph_ga --seed 0 --output_dir eval/results
+python mol_opt/run_drug_nlp.py $1  graph_ga --seed 0 --output_dir eval/results
+# python mol_opt/run_drug_nlp.py final_finetuning/results/10_production_experiment.csv  graph_ga --seed 0 --output_dir eval/results
 # python mol_opt/run_drug_nlp.py final_finetuning/results/50_train_experiment.csv  graph_ga --seed 0 --output_dir eval/results
